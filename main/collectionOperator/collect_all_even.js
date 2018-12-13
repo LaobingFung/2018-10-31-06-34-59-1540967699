8b139353fe048a5ee7d87c0544990f24dc56fdfd
LaoBing_Fung <FLB_B401@163.com>
2018-12-13 21:16:31 +0800

    collect all even in an array

--- main/collectionOperator/collect_all_even.js
+++ main/collectionOperator/collect_all_even.js
@@ -2,6 +2,13 @@
 
 function collect_all_even(collection) {
   //在这里写入代码
+  let even_arr = [];
+  for(let i = 0; i < collection.length; i++) {
+    if(collection[i] % 2 === 0){
+        even_arr.push(collection[i]);
+    }
+  }
+  return even_arr;
 }
 
 module.exports = collect_all_even;
