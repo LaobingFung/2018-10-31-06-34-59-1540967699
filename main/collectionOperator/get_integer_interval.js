b3495bf2d0507deb4e5fa49f6eb9e760e44b4891
LaoBing_Fung <FLB_B401@163.com>
2018-12-13 22:02:03 +0800

    return even array from number a to b

--- main/collectionOperator/get_integer_interval.js
+++ main/collectionOperator/get_integer_interval.js
@@ -2,7 +2,18 @@
 
 function get_integer_interval(number_a, number_b) {
   //在这里写入代码
+  let even_array;
+  if (number_a < number_b) {
+    return get_even_interval_inc(number_a, number_b);
+  }
+  return get_even_interval_inc(number_b, number_a).reverse();
 }
 
+function get_even_interval_inc(min, max) {
+  let even_array = [];
+  for (let i = min; i <= max; i++) {
+    even_array.push(i);
+  }
+  return even_array;
+}
 module.exports = get_integer_interval;
-
--- main/collectionOperator/get_integer_interval_2.js
+++ main/collectionOperator/get_integer_interval_2.js
@@ -2,6 +2,19 @@
 
 function get_integer_interval_2(number_a, number_b) {
   //在这里写入代码
+  if (number_a < number_b) {
+    return get_even_interval_inc(number_a, number_b);
+  }
+  return get_even_interval_inc(number_b, number_a).reverse();
 }
 
+function get_even_interval_inc(min, max) {
+  let even_array = [];
+  for (let i = min; i <= max; i++) {
+    if (i % 2 === 0) {
+      even_array.push(i);
+    }
+  }
+  return even_array;
+}
 module.exports = get_integer_interval_2;
