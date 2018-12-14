73bf6f2ce7cdc4d3194475abfa7cfced6a897ef0
LaoBing_Fung <FLB_B401@163.com>
2018-12-14 11:06:58 +0800

    get nonredundant sub array from an integer array

--- main/filter/choose_no_repeat_number.js
+++ main/filter/choose_no_repeat_number.js
@@ -1,8 +1,10 @@
 'use strict';
 
 function choose_no_repeat_number(collection) {
-
   //在这里写入代码
+  let no_repeat_collection = collection.filter(function(element, i, arr) {
+    return arr.lastIndexOf(element) === i;
+  })
+  return no_repeat_collection;
 }
-
 module.exports = choose_no_repeat_number;
