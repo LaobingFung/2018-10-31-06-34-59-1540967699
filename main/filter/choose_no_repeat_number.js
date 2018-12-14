9e1acad40a9ea4d8c2fba7680610490cbaeaf976
LaoBing_Fung <FLB_B401@163.com>
2018-12-14 20:36:55 +0800

    add another method to select nonredundant elements

--- main/filter/choose_no_repeat_number.js
+++ main/filter/choose_no_repeat_number.js
@@ -1,10 +1,19 @@
 'use strict';
 
+// function choose_no_repeat_number(collection) {
+//   //在这里写入代码
+//   let no_repeat_collection = collection.filter(function(element, i, arr) {
+//     return arr.lastIndexOf(element) === i;
+//   })
+//   return no_repeat_collection;
+// }
 function choose_no_repeat_number(collection) {
-  //在这里写入代码
-  let no_repeat_collection = collection.filter(function(element, i, arr) {
-    return arr.lastIndexOf(element) === i;
-  })
+  let no_repeat_collection = [];
+  for (let element of collection) {
+    if (!no_repeat_collection.includes(element)) {
+      no_repeat_collection.push(element);
+    }
+  }
   return no_repeat_collection;
 }
 module.exports = choose_no_repeat_number;
