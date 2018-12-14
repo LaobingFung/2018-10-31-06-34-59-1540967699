4ce4495db3b2804835b2971e331ea6d5e2ec149a
LaoBing_Fung <FLB_B401@163.com>
2018-12-14 19:21:52 +0800

    get intersection from 2 collections

--- main/filter/two_collections_practice_1.js
+++ main/filter/two_collections_practice_1.js
@@ -1,8 +1,14 @@
 'use strict';
 
 function choose_common_elements(collection_a, collection_b) {
-
   //在这里写入代码
+  let intersection = new Array();
+  for(let element_a of collection_a){
+    if (collection_b.includes(element_a)){
+        intersection.push(element_a);
+    }
+  }
+  return intersection;
 }
 
 module.exports = choose_common_elements;
