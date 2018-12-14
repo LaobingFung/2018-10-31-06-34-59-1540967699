b129ed5d8bc648960428a17d1aadd4a522f5f6c1
LaoBing_Fung <FLB_B401@163.com>
2018-12-14 10:42:47 +0800

    get union of two collections

--- main/collectionOperator/get_union.js
+++ main/collectionOperator/get_union.js
@@ -2,6 +2,13 @@
 
 function get_union(collection_a, collection_b) {
   //在这里写入代码
+  let union = collection_a.slice(0);
+  collection_b.forEach(function(element_b){
+    if (!collection_a.includes(element_b)){
+        union.push(element_b);
+    }
+  })
+  return union;
 }
 
 module.exports = get_union;
