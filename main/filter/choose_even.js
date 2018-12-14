15ebba527a1b526159cca96f9b591f141200624a
LaoBing_Fung <FLB_B401@163.com>
2018-12-14 10:50:40 +0800

    get even elements from an integer array

--- main/filter/choose_even.js
+++ main/filter/choose_even.js
@@ -1,8 +1,11 @@
 'use strict';
 
 function choose_even(collection) {
-
   //在这里写入代码
+  let even_collection = collection.filter(function (element) {
+    return element % 2 === 0;
+  })
+  return even_collection;
 }
 
 module.exports = choose_even;
