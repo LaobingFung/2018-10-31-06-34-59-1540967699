215802af55246347d03eb3b40a2e43242ef97940
LaoBing_Fung <FLB_B401@163.com>
2018-12-14 19:33:14 +0800

    get sub collection of a divisible by b

--- main/filter/two_collections_practice_3.js
+++ main/filter/two_collections_practice_3.js
@@ -1,8 +1,15 @@
 'use strict';
 
 function choose_divisible_integer(collection_a, collection_b) {
-
   //在这里写入代码
+  let sub_a_divisible_by_b = new Array();
+  for (let element_a of collection_a) {
+    for (let element_b of collection_b) {
+      if ((element_a % element_b === 0) && !sub_a_divisible_by_b.includes(element_a)) {
+        sub_a_divisible_by_b.push(element_a);
+      }
+    }
+  }
+  return sub_a_divisible_by_b;
 }
-
 module.exports = choose_divisible_integer;
