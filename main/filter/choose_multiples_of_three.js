eb0fad7a88aef91c8ad23baccbb14626a201e9d2
LaoBing_Fung <FLB_B401@163.com>
2018-12-14 10:54:18 +0800

    get multiples of 3 from an integer array

--- main/filter/choose_multiples_of_three.js
+++ main/filter/choose_multiples_of_three.js
@@ -1,8 +1,11 @@
 'use strict';
 
 function choose_multiples_of_three(collection) {
-
   //在这里写入代码
+  let times_of_3_collection = collection.filter(function (element) {
+    return element % 3 === 0;
+  })
+  return times_of_3_collection;
 }
 
 module.exports = choose_multiples_of_three;
