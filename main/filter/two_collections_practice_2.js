4897f0de2cc558d641c189dee319965028c805e4
LaoBing_Fung <FLB_B401@163.com>
2018-12-14 19:27:17 +0800

    get sub collection of a except elements in b

--- main/filter/two_collections_practice_2.js
+++ main/filter/two_collections_practice_2.js
@@ -1,8 +1,13 @@
 'use strict';
 
 function choose_no_common_elements(collection_a, collection_b) {
-
   //在这里写入代码
+  let sub_a_except_b = new Array();
+  for (let element_a of collection_a) {
+    if (!collection_b.includes(element_a)) {
+      sub_a_except_b.push(element_a);
+    }
+  }
+  return sub_a_except_b;
 }
-
 module.exports = choose_no_common_elements;
