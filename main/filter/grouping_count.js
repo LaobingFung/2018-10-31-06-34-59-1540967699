9f6f731cae703b27aafffcae5d21e91d473e3e39
LaoBing_Fung <FLB_B401@163.com>
2018-12-14 13:01:42 +0800

    count nunredundant elements in an collection

--- main/filter/grouping_count.js
+++ main/filter/grouping_count.js
@@ -1,8 +1,20 @@
 'use strict';
 
+var choose_no_repeat_number = require("../filter/choose_no_repeat_number.js");
+
 function grouping_count(collection) {
-
   //在这里写入代码
+  let element_count = new Object;
+  let no_repeat_array = choose_no_repeat_number(collection);
+  for (let i = 0; i < no_repeat_array.length; i++) {
+    let count = 0;
+    for (let j = 0; j < collection.length; j++) {
+      if (no_repeat_array[i] === collection[j]) {
+        count++;
+      }
+    }
+    element_count[no_repeat_array[i]] = count;
+  }
+  return element_count;
 }
-
 module.exports = grouping_count;
