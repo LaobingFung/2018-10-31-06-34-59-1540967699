1edfe11453ded89acbe9088c596d634b93cf71d0
LaoBing_Fung <FLB_B401@163.com>
2018-12-15 11:14:51 +0800

    triple an array

--- main/map/map_to_three_multiples.js
+++ main/map/map_to_three_multiples.js
@@ -1,6 +1,9 @@
 'use strict';
-var map_to_three_multiples = function(collections){
-  return [3,9,15,12,27];
+var map_to_three_multiples = function(collection) {
+  let three_times = new Array();
+  for (let element of collection) {
+    three_times.push(element * 3);
+  }
+  return three_times;
 };
-
 module.exports = map_to_three_multiples;
