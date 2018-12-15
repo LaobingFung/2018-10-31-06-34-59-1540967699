ff87eb3367be5070af5129225737990b10e332b3
LaoBing_Fung <FLB_B401@163.com>
2018-12-15 21:46:44 +0800

    count same elements of an array

--- main/section_2/practice_1/practice.js
+++ main/section_2/practice_1/practice.js
@@ -1,5 +1,23 @@
+'use strict';
+
 function count_same_elements(collection) {
   //在这里写入代码
+  let count_result = [];
+  let counter = count_element(collection);
+  for (let object of counter) {
+    count_result.push({key: object[0], count: object[1]});
+  }
+  return count_result;
 }
 
+function count_element(array) {
+  let counter_map = new Map();
+  for (let element of array) {
+    if (!counter_map.has(element)) {
+      counter_map.set(element, 0);
+    }
+    counter_map.set(element, counter_map.get(element) + 1);
+  }
+  return counter_map;
+}
 module.exports = count_same_elements;
