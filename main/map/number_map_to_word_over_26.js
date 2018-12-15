6d2d264a6bd59593ed971f1a24c59d9f43efcccd
LaoBing_Fung <FLB_B401@163.com>
2018-12-15 17:23:06 +0800

    convert decimal numbers to 26 system

--- main/map/number_map_to_word_over_26.js
+++ main/map/number_map_to_word_over_26.js
@@ -1,6 +1,25 @@
 'use strict';
-var number_map_to_word_over_26 = function(collection){
-  return ['a','m','aa','ad','y','aa'];
+var number_map_to_word = require('../map/number_map_to_word.js');
+var number_map_to_word_over_26 = function(collection) {
+  let words_collection = [];
+  for (let element of collection) {
+    let arr26 = decimal_to_26system_arr(element);
+    let word_over26 = number_map_to_word(arr26).join('');
+    words_collection.push(word_over26);
+  }
+  return words_collection;
 };
 
+function decimal_to_26system_arr(number) {
+  let arr26 = [];
+  if (number <= 26) {
+    arr26.push(number);
+    return arr26;
+  }
+  while (number / 26 !== 0) {
+    arr26.push(number % 26);
+    number = Math.floor(number / 26);
+  }
+  return arr26.reverse();
+}
 module.exports = number_map_to_word_over_26;
