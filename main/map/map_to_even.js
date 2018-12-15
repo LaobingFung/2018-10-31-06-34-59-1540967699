66b023adca9383177b75f1762fce56a72bdd4020
LaoBing_Fung <FLB_B401@163.com>
2018-12-15 10:59:58 +0800

    double a array

--- main/map/map_to_even.js
+++ main/map/map_to_even.js
@@ -1,5 +1,10 @@
 'use strict';
-function map_to_even(collection){
-  return [2,4,6,8,10];
+
+function map_to_even(collection) {
+  let double = new Array();
+  for (let element of collection) {
+    double.push(element * 2);
+  }
+  return double;
 }
 module.exports = map_to_even;
