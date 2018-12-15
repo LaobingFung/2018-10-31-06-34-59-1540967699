f0b6f9ed4375e373fb8b9f300d775580ab6fb441
LaoBing_Fung <FLB_B401@163.com>
2018-12-15 11:19:24 +0800

    convert an array to four times and add one

--- main/map/map_to_four_multiples_add_one.js
+++ main/map/map_to_four_multiples_add_one.js
@@ -1,6 +1,9 @@
 'use strict';
-var map_to_four_multiples_add_one = function(collection){
-  return [5,9,13,17,21];
+var map_to_four_multiples_add_one = function(collection) {
+  let four_times_add_one = new Array();
+  for (let element of collection) {
+    four_times_add_one.push(element * 4 + 1);
+  }
+  return four_times_add_one;
 };
-
 module.exports = map_to_four_multiples_add_one;
