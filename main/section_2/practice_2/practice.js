63b770c88b85a20ec0b9822141cfa402c306e943
LaoBing_Fung <FLB_B401@163.com>
2018-12-15 23:11:19 +0800

    count same elements in an array by using regexp

--- main/section_2/practice_2/practice.js
+++ main/section_2/practice_2/practice.js
@@ -1,5 +1,37 @@
+'use strict';
+
 function count_same_elements(collection) {
   //在这里写入代码
+  let count_result = [];
+  let counter = count_element(collection);
+  for (let object of counter) {
+    count_result.push({
+      key: object[0],
+      count: object[1]
+    });
+  }
+  return count_result;
 }
 
+function count_element(array) {
+  let counter_map = new Map();
+  let re_bar = /[A-z]-[1-9][0-9]*/;
+  let re_colon = /[A-z]:[1-9][0-9]*/;
+  let re_sqbracket = /[A-z]\[[1-9][0-9]*\]/
+  for (let element of array) {
+    if (re_bar.test(element) || re_colon.test(element) || re_sqbracket.test(element)) {
+      add_count_map(counter_map, element.match(/[A-z]/)[0], parseInt(element.match(/[1-9][0-9]*/)[0]))
+      continue;
+    }
+    add_count_map(counter_map, element, 1);
+  }
+  return counter_map;
+}
+
+function add_count_map(map, element, count) {
+  if (!map.has(element)) {
+    map.set(element, 0);
+  }
+  map.set(element, map.get(element) + count);
+}
 module.exports = count_same_elements;
