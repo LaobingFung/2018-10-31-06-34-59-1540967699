3131b3de98bd9107899a4424b3bf5daca2a48a57
LaoBing_Fung <FLB_B401@163.com>
2018-12-15 18:57:09 +0800

    get intersection from 2 collections

--- main/section_1/practice_1/practice.js
+++ main/section_1/practice_1/practice.js
@@ -1,5 +1,13 @@
+'use strict';
+
 function collect_same_elements(collection_a, collection_b) {
   //在这里写入代码
+  let intersection = [];
+  for (let element_a of collection_a) {
+    if (collection_b.includes(element_a)) {
+      intersection.push(element_a);
+    }
+  }
+  return intersection;
 }
-
 module.exports = collect_same_elements;
