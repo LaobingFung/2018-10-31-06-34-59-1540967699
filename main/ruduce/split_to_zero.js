631ad2726d37a457839bfaf12438422614f2bdbd
LaoBing_Fung <FLB_B401@163.com>
2018-12-15 18:34:04 +0800

    get an interval array to zero or less than zero

--- main/ruduce/split_to_zero.js
+++ main/ruduce/split_to_zero.js
@@ -2,6 +2,11 @@
 
 function spilt_to_zero(number, interval) {
   //在这里写入代码
+  let interval_arr = [];
+  for (; number + interval > 0; number -= interval) {
+    number = Math.round(number * 10) / 10;
+    interval_arr.push(number);
+  }
+  return interval_arr;
 }
-
 module.exports = spilt_to_zero;
