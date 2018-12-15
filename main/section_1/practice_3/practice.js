244baad87ab099691de821d351f89b13dc3c3c7f
LaoBing_Fung <FLB_B401@163.com>
2018-12-15 20:54:47 +0800

    get intersection from an array and a member array of object

--- main/section_1/practice_3/practice.js
+++ main/section_1/practice_3/practice.js
@@ -1,5 +1,14 @@
+'use strict'
+
 function collect_same_elements(collection_a, object_b) {
   //在这里写入代码
+  let intersection = [];
+  for (let element_a of collection_a) {
+    if (object_b["value"].includes(element_a)) {
+      intersection.push(element_a);
+    }
+  }
+  return intersection;
 }
 
 module.exports = collect_same_elements;
