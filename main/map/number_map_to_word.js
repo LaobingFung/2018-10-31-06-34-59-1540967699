877831512e5373620b3f9282c3238b4529973a25
LaoBing_Fung <FLB_B401@163.com>
2018-12-15 16:28:44 +0800

    convert numbers less than 26 to letters

--- main/map/number_map_to_word.js
+++ main/map/number_map_to_word.js
@@ -1,6 +1,18 @@
 'use strict';
+
+const alpha_number = {
+    '1': 'a', '2': 'b', '3': 'c', '4': 'd', '5': 'e',
+    '6': 'f', '7': 'g', '8': 'h', '9': 'i', '10': 'j',
+    '11': 'k', '12': 'l', '13': 'm', '14': 'n', '15': 'o',
+    '16': 'p', '17': 'q', '18': 'r', '19': 's', '20': 't',
+    '21': 'u', '22': 'v', '23': 'w', '24': 'x', '25': 'y', '26': 'z'
+}
 var number_map_to_word = function(collection){
-  return ['a','b','c','d','e'];
-};
+  let words_collection = [];
+  for(let element of collection) {
+    words_collection.push(alpha_number[element]);
+  }
+  return words_collection;
+}
 
 module.exports = number_map_to_word;
