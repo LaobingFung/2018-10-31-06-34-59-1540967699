c9fd72484dee1ec865d0b5f1b1d7f76b8a0b121e
LaoBing_Fung <FLB_B401@163.com>
2018-12-16 17:22:26 +0800

    map average of an array to letter

--- main/superposition_operation/collection_operation/letter_mapping_2.js
+++ main/superposition_operation/collection_operation/letter_mapping_2.js
@@ -1,8 +1,13 @@
 'use strict';
+var number_map_to_word_over_26 = require('../../map/number_map_to_word_over_26.js');
+var compute_average = require('../../reduce/compute_average.js');
+
 
 function average_to_letter(collection) {
-
   //在这里写入代码
+  let avg = compute_average(collection);
+  let letter = number_map_to_word_over_26([Math.ceil(avg)]);
+  return letter[0];
 }
 
 module.exports = average_to_letter;
