4e3ca09e92826e3b3cfe6c7283ab0457cd27398f
LaoBing_Fung <FLB_B401@163.com>
2018-12-16 13:18:59 +0800

    compute median of an array

--- main/reduce/compute_median.js
+++ main/reduce/compute_median.js
@@ -2,8 +2,12 @@
 
 function compute_median(collection) {
   //在这里写入代码
+  let sort_arr = collection.sort(function(a, b) {
+    return b - a;
+  });
+  if (collection.length % 2 === 1) {
+    return sort_arr[Math.floor(sort_arr.length / 2)];
+  }
+  return (sort_arr[sort_arr.length / 2] + sort_arr[sort_arr.length / 2 - 1]) / 2;
 }
-
 module.exports = compute_median;
-
-
