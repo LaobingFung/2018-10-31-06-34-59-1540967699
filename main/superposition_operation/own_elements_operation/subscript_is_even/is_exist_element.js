9f5dd0d3038186406d6bcf590cfd99846d4569fa
LaoBing_Fung <FLB_B401@163.com>
2018-12-16 21:06:15 +0800

    judge if the even subscript sub array includes an element

--- main/superposition_operation/own_elements_operation/subscript_is_even/is_exist_element.js
+++ main/superposition_operation/own_elements_operation/subscript_is_even/is_exist_element.js
@@ -1,5 +1,19 @@
 'use strict';
-var is_exist_element = function(collection,element){
 
-};
+var is_exist_element = function(collection, element) {
+  let even_subscript_arr = get_subscript_arr(collection, "even");
+  return even_subscript_arr.includes(element);
+}
+
+function get_subscript_arr(collection, script_type) {
+  if (script_type === "even") {
+    return collection.filter(function(cur_value, cur_i, arr) {
+      return cur_i % 2 === 0;
+    })
+  } else if (script_type === "odd") {
+    return collection.filter(function(cur_value, cur_i, arr) {
+      return cur_i % 2 === 1;
+    })
+  }
+}
 module.exports = is_exist_element;
