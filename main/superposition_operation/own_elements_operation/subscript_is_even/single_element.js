2783f26c4d277ae8b8b2c8cd0c87d12dab3a7cce
LaoBing_Fung <FLB_B401@163.com>
2018-12-16 21:19:07 +0800

    get elements that appear only once of odd subscript array

--- main/superposition_operation/own_elements_operation/subscript_is_even/single_element.js
+++ main/superposition_operation/own_elements_operation/subscript_is_even/single_element.js
@@ -1,5 +1,22 @@
 'use strict';
-var single_element = function(collection){
 
-};
+var single_element = function(collection) {
+  let odd_subscript_arr = get_subscript_arr(collection, "odd");
+  let single_arr = odd_subscript_arr.filter(function(cur_value, cur_i, arr) {
+    return arr.indexOf(cur_value) === arr.lastIndexOf(cur_value);
+  })
+  return single_arr;
+}
+
+function get_subscript_arr(collection, script_type) {
+  if (script_type === "even") {
+    return collection.filter(function(cur_value, cur_i, arr) {
+      return cur_i % 2 === 0;
+    })
+  } else if (script_type === "odd") {
+    return collection.filter(function(cur_value, cur_i, arr) {
+      return cur_i % 2 === 1;
+    })
+  }
+}
 module.exports = single_element;
