681cb970d15b7859d546d98c417e328dc93ec8d0
LaoBing_Fung <FLB_B401@163.com>
2018-12-16 20:50:08 +0800

    calculate average of odd subscript element

--- main/superposition_operation/own_elements_operation/subscript_is_even/calculate_average.js
+++ main/superposition_operation/own_elements_operation/subscript_is_even/calculate_average.js
@@ -1,5 +1,19 @@
 'use strict';
-var calculate_average = function(collection){
+var compute_average = require("../../../reduce/compute_average.js");
+var calculate_average = function(collection) {
+  let odd_subscript_arr = get_subscript_arr(collection, "odd");
+  return compute_average(odd_subscript_arr);
+}
 
-};
+function get_subscript_arr(collection, script_type) {
+  if (script_type === "even") {
+    return collection.filter(function(cur_value, cur_i, arr) {
+      return cur_i % 2 === 0;
+    })
+  } else if (script_type === "odd") {
+    return collection.filter(function(cur_value, cur_i, arr) {
+      return cur_i % 2 === 1;
+    })
+  }
+}
 module.exports = calculate_average;
