863da676cc7ec8d8fbfc430a4e0930362e092b06
LaoBing_Fung <FLB_B401@163.com>
2018-12-16 18:20:09 +0800

    get sum of even numbers

--- main/superposition_operation/interval_operation/amount_even.js
+++ main/superposition_operation/interval_operation/amount_even.js
@@ -1,8 +1,10 @@
 'use strict';
+var choose_even = require("../../filter/choose_even.js");
+var compute_elements_sum = require('../../reduce/compute_sum.js');
 
 function amount_even(collection) {
-
   //在这里写入代码
+  let even = choose_even(collection);
+  return compute_elements_sum(even);
 }
-
 module.exports = amount_even;
