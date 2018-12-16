ecaa90547723970b91b40d346bf57aba3094eb81
LaoBing_Fung <FLB_B401@163.com>
2018-12-16 12:08:26 +0800

    compute sum of an array

--- main/reduce/compute_sum.js
+++ main/reduce/compute_sum.js
@@ -2,7 +2,10 @@
 
 function calculate_elements_sum(collection) {
   //在这里写入代码
+  return collection.reduce(get_sum, 0);
 }
 
+function get_sum(sum, num) {
+  return sum += num;
+}
 module.exports = calculate_elements_sum;
-
