3f9482624c1d9e5c47899eb31f5074866c6b994c
LaoBing_Fung <FLB_B401@163.com>
2018-12-16 18:32:52 +0800

    map median of an array to 26 number system letter

--- main/superposition_operation/interval_operation/median_to_letter.js
+++ main/superposition_operation/interval_operation/median_to_letter.js
@@ -1,8 +1,12 @@
 'use strict';
+var number_map_to_word_over_26 = require('../../map/number_map_to_word_over_26.js');
+var compute_median = require('../../reduce/compute_median.js');
 
 function median_to_letter(collection) {
-
   //在这里写入代码
+  let median = compute_median(collection);
+  let letter = number_map_to_word_over_26([Math.ceil(median)]);
+  return letter[0];
 }
 
 module.exports = median_to_letter;
