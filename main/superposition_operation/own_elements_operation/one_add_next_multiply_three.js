d3f848d02a1a79faf9148cc1d8cf26f064c08250
LaoBing_Fung <FLB_B401@163.com>
2018-12-16 19:01:19 +0800

    map an array to 3 times of sum of 2 contiguous numbers

--- main/superposition_operation/own_elements_operation/one_add_next_multiply_three.js
+++ main/superposition_operation/own_elements_operation/one_add_next_multiply_three.js
@@ -1,5 +1,10 @@
 'use strict';
-function one_add_next_multiply_three(collection){
-  return [12,24,36,48,60,72,84,96,108,120];
+
+function one_add_next_multiply_three(collection) {
+  let map_arr = collection.map(function(cur_value, cur_i, arr) {
+    return (cur_value + arr[cur_i + 1]) * 3;
+  })
+  map_arr.pop();
+  return map_arr;
 }
 module.exports = one_add_next_multiply_three;
