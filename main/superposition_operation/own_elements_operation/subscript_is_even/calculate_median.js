60bfaa86f8aaad92bbd358e1a5280a5e2ea24518
LaoBing_Fung <FLB_B401@163.com>
2018-12-16 20:57:12 +0800

    calculate median of odd subscript element

--- main/superposition_operation/own_elements_operation/subscript_is_even/calculate_median.js
+++ main/superposition_operation/own_elements_operation/subscript_is_even/calculate_median.js
@@ -1,5 +1,21 @@
 'use strict';
-var calculate_median = function(collection){
 
+var compute_median = require("../../../reduce/compute_median.js")
+
+var calculate_median = function(collection) {
+  let odd_subscript_arr = get_subscript_arr(collection, "odd");
+  return compute_median(odd_subscript_arr);
 };
+
+function get_subscript_arr(collection, script_type) {
+  if (script_type === "even") {
+    return collection.filter(function(cur_value, cur_i, arr) {
+      return cur_i % 2 === 0;
+    })
+  } else if (script_type === "odd") {
+    return collection.filter(function(cur_value, cur_i, arr) {
+      return cur_i % 2 === 1;
+    })
+  }
+}
 module.exports = calculate_median;
