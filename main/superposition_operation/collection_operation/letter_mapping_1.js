8cc84020097e02710731e4c585832dad1019f733
LaoBing_Fung <FLB_B401@163.com>
2018-12-16 17:15:02 +0800

    map even numbers to letters

--- main/superposition_operation/collection_operation/letter_mapping_1.js
+++ main/superposition_operation/collection_operation/letter_mapping_1.js
@@ -1,8 +1,11 @@
 'use strict';
+var number_map_to_word_over_26 = require('../../map/number_map_to_word_over_26.js');
+var choose_even = require("../../filter/choose_even.js");
 
 function even_to_letter(collection) {
-
   //在这里写入代码
+  let even = choose_even(collection);
+  return number_map_to_word_over_26(even);
 }
 
 module.exports = even_to_letter;
