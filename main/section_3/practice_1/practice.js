b364bdd7fbe5770698b306dff4bb6694027d036b
LaoBing_Fung <FLB_B401@163.com>
2018-12-16 14:12:16 +0800

    complete a function to update a count list

--- main/section_3/practice_1/practice.js
+++ main/section_3/practice_1/practice.js
@@ -1,5 +1,17 @@
+'use strict';
+var count_same_element = require("../../section_2/practice_2/practice.js");
+
 function create_updated_collection(collection_a, object_b) {
   //在这里写入代码
+  let count_list_b = count_same_element(object_b.value);
+  let collection_c = collection_a.slice(0);
+  for (let count_item_b of count_list_b) {
+    for (let count_item_c of collection_c) {
+      if (count_item_b.key === count_item_c.key) {
+        count_item_c.count -= count_item_b.count;
+      }
+    }
+  }
+  return collection_c;
 }
-
 module.exports = create_updated_collection;
