1fe3d9b9c4ac69be594e53a988c3ccaf6d732d7d
LaoBing_Fung <FLB_B401@163.com>
2018-12-16 11:28:56 +0800

    judge if 2 collections are equal

--- main/reduce/compare_collections.js
+++ main/reduce/compare_collections.js
@@ -2,8 +2,14 @@
 
 function compare_collections(collection_a, collection_b) {
   //在这里写入代码
+  // let equal = true;
+  let keys_a = Object.getOwnPropertyNames(collection_a);
+  let keys_b = Object.getOwnPropertyNames(collection_b);
+  for (let i = 0; i < keys_a.length; i++) {
+    if (collection_a[keys_a[i]] !== collection_b[keys_b[i]]) {
+      return false;
+    }
+  }
+  return true;
 }
-
 module.exports = compare_collections;
-
-
