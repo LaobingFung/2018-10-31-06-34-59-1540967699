7f3f985fef96863e4fb348e1193eb0c38ffe1f2f
LaoBing_Fung <FLB_B401@163.com>
2018-12-16 17:30:08 +0800

    map an array to 3 times and add 2 then get sum

--- main/superposition_operation/collection_operation/practice_1.js
+++ main/superposition_operation/collection_operation/practice_1.js
@@ -1,9 +1,11 @@
 'use strict';
+var compute_elements_sum = require('../../reduce/compute_sum.js');
 
 function hybrid_operation(collection) {
-
   //在这里写入代码
+  let map_arr = collection.map(function(cur_value) {
+    return cur_value * 3 + 2;
+  })
+  return compute_elements_sum(map_arr);
 }
-
 module.exports = hybrid_operation;
-
