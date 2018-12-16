8f0552d19995b9ef59b380800b05052f4d24b582
LaoBing_Fung <FLB_B401@163.com>
2018-12-16 11:14:34 +0800

    get minimum of an array

--- main/reduce/collect_min_number.js
+++ main/reduce/collect_min_number.js
@@ -2,7 +2,10 @@
 
 function collect_min_number(collection) {
   //在这里写入代码
+  return collection.reduce(get_min, 1000);
 }
 
+function get_min(min, num) {
+  return min < num ? min : num;
+}
 module.exports = collect_min_number;
-
