6aab6b840786abb95c1f694bb2996cd4805f93b0
LaoBing_Fung <FLB_B401@163.com>
2018-12-16 13:25:16 +0800

    compute median of a chain string

--- main/reduce/compute_chain_median.js
+++ main/reduce/compute_chain_median.js
@@ -1,7 +1,12 @@
 'use strict';
+let get_median = require('../reduce/compute_median.js');
 
 function compute_chain_median(collection) {
   //在这里写入代码
+  let arr_str = collection.split('->');
+  let arr = arr_str.map(function(num_str) {
+    return parseInt(num_str);
+  })
+  return get_median(arr);
 }
-
 module.exports = compute_chain_median;
