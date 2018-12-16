7606dcf020ad46dbdf2cb35f174811fb780465a5
LaoBing_Fung <FLB_B401@163.com>
2018-12-16 11:11:59 +0800

    get maximum of an array

--- main/reduce/collect_max_number.js
+++ main/reduce/collect_max_number.js
@@ -2,6 +2,10 @@
 
 function collect_max_number(collection) {
   //在这里写入代码
+  return collection.reduce(get_max, 5);
 }
 
+function get_max(max, num) {
+  return max > num ? max : num;
+}
 module.exports = collect_max_number;
