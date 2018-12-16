6a2f302cffba27814a476fca80b6588a3f32422c
LaoBing_Fung <FLB_B401@163.com>
2018-12-16 20:19:23 +0800

    concat asc even sub array and desc odd sub array

--- main/superposition_operation/own_elements_operation/even_asc_odd_desc.js
+++ main/superposition_operation/own_elements_operation/even_asc_odd_desc.js
@@ -1,5 +1,15 @@
 'use strict';
-var even_asc_odd_desc = function(collection){
 
-};
+var choose_no_common_elements = require("../../filter/two_collections_practice_2.js");
+var choose_even = require("../../filter/choose_even.js");
+var rank_asc = require('../../map/rank_asc.js');
+var rank_desc = require('../../map/rank_desc.js');
+
+var even_asc_odd_desc = function(collection) {
+  let even_arr = choose_even(collection);
+  let odd_arr = choose_no_common_elements(collection, even_arr);
+  let rank_even_asc = rank_asc(even_arr);
+  let rank_odd_desc = rank_desc(odd_arr);
+  return rank_even_asc.concat(rank_odd_desc);
+}
 module.exports = even_asc_odd_desc;
