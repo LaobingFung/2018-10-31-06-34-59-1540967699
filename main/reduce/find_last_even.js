dca334a1e582a379b8023f95ce252204c16fd44a
LaoBing_Fung <FLB_B401@163.com>
2018-12-16 13:44:33 +0800

    find last even in an array

--- main/reduce/find_last_even.js
+++ main/reduce/find_last_even.js
@@ -2,6 +2,11 @@
 
 function find_last_even(collection) {
   //在这里写入代码
+  for (let element of collection.reverse()) {
+    if (element % 2 === 0) {
+      return element;
+    }
+  }
+  return undefined;
 }
-
 module.exports = find_last_even;
