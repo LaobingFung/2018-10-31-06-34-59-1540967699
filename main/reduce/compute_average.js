073e78dcd7f3ec70d267a488ef0b8baa0ce212e5
LaoBing_Fung <FLB_B401@163.com>
2018-12-16 12:57:12 +0800

    compute average of an array

--- main/reduce/compute_average.js
+++ main/reduce/compute_average.js
@@ -2,7 +2,10 @@
 
 function compute_average(collection) {
   //在这里写入代码
+  return collection.reduce(get_sum) / collection.length;
 }
 
+function get_sum(sum, num) {
+  return sum += num;
+}
 module.exports = compute_average;
-
